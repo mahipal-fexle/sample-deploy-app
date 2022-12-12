daf8978d93a50fe2c7e8d979879f9a6aafeda847
Mahipal Singh <mahipal@github.com>
2022-12-12 15:07:31 +0530

    Added Some configuration changes and new version

--- scripts/after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'run after_install.sh: ' >> /home/ubuntu/sample-deploy-app/deploy.log
-
-echo 'cd /home/ubuntu/nodejs-server-cicd' >> /home/ubuntu/sample-deploy-app/deploy.log
-cd /home/ubuntu/sample-deploy-app >> /home/ubuntu/sample-deploy-app/deploy.log
-
-echo 'npm install' >> /home/ubuntu/sample-deploy-app/deploy.log 
-npm install >> /home/ubuntu/sample-deploy-app/deploy.log
--- scripts/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'run application_start.sh: ' >> /home/ubuntu/sample-deploy-app/deploy.log
-
-echo 'pm2 restart uat-sample-app' >> /home/ubuntu/sample-deploy-app/deploy.log
-pm2 restart uat-sample-app >> /home/ubuntu/sample-deploy-app/deploy.log
